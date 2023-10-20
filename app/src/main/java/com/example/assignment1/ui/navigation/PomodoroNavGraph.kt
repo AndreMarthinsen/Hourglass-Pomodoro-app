832fc4ea4875196d76ed4596af0ff2abf79318e4
Even Stetrud <evenstet@stud.ntnu.no>
2023-10-21 01:31:21 +0200

    Removed routing to dedicated NavGraph-file

---
+++ app/src/main/java/com/example/assignment1/ui/navigation/PomodoroNavGraph.kt
@@ -0,0 +1,48 @@
+package com.example.assignment1.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.NavHostController
+import com.example.assignment1.ui.preset.ActivePresetDestination
+import com.example.assignment1.ui.preset.ActiveTimerScreen
+import com.example.assignment1.ui.preset.PresetEditDestination
+import com.example.assignment1.ui.preset.PresetEditScreen
+import com.example.assignment1.ui.preset.PresetsDestination
+import com.example.assignment1.ui.preset.PresetsScreen
+import com.example.assignment1.ui.settings.SettingsDestination
+import com.example.assignment1.ui.settings.SettingsScreen
+
+@Composable
+fun PomodoroNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = PresetsDestination.route,
+        modifier = modifier
+    ) {
+        composable(route = ActivePresetDestination.route) {
+            ActiveTimerScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+        composable(route = PresetEditDestination.route) {
+            PresetEditScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+        composable(route = PresetsDestination.route) {
+            PresetsScreen(
+                navigateToPresetEdit = { navController.navigate(PresetEditDestination.route)}
+            )
+        }
+        composable(route = SettingsDestination.route) {
+            SettingsScreen(
+                navigateBack = { navController.popBackStack() },
+                )
+        }
+    }
+}

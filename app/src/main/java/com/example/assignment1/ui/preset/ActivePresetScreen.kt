33ee302a4e0a64f6f3b67cc2203de5a842846889
andrrma <andrrma@stud.ntnu.no>
2023-10-22 12:04:53 +0200

    Adds initial TimerService

--- app/src/main/java/com/example/assignment1/ui/preset/ActivePresetScreen.kt
+++ app/src/main/java/com/example/assignment1/ui/preset/ActivePresetScreen.kt
@@ -1,17 +1,61 @@
 package com.example.assignment1.ui.preset
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.IBinder
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.assignment1.PomodoroTopAppBar
+import com.example.assignment1.R
+import com.example.assignment1.services.TimerService
+import com.example.assignment1.services.formatTime
 import com.example.assignment1.ui.navigation.NavigationDestination
+import kotlinx.coroutines.launch
+import kotlin.math.abs
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.DurationUnit
 
 /**
  * the navigation destination for the ActivePreset (active timer) screen
@@ -60,3 +104,181 @@ fun ActiveTimerScreen(
 fun ActiveTimerBody(modifer: Modifier = Modifier) {
 
 }
+
+
+//@ExperimentalAnimationApi
+//class MainActivity : ComponentActivity() {
+//
+//    private var isBound = mutableStateOf(false)
+//    private lateinit var stopwatchService: TimerService
+//
+//    private val connection = object : ServiceConnection {
+//        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+//            val binder = service as TimerService.TimerBinder
+//            stopwatchService = binder.getService()
+//            isBound.value = true
+//        }
+//
+//        override fun onServiceDisconnected(arg0: ComponentName) {
+//            isBound.value = false
+//        }
+//    }
+//
+//    override fun onStart() {
+//        super.onStart()
+//    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        Intent(this, TimerService::class.java).also { intent ->
+//            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+//        }
+//        setContent {
+//            if(isBound.value) {
+//                MainScreen(timerService = stopwatchService)
+//            }
+//        }
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        unbindService(connection)
+//        isBound.value = false
+//    }
+//}
+
+//@Composable
+//fun MainScreen(timerService: TimerService) {
+//    Column(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .background(MaterialTheme.colorScheme.surface)
+//            .padding(30.dp),
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//
+//        val scrollScope = rememberCoroutineScope()
+//        val scrollState = rememberScrollState(
+//            timerService.currentTimeInSeconds.value.absoluteValue.toInt(DurationUnit.SECONDS)
+//        )
+//        var timerFinished by remember { mutableStateOf( false ) }
+//
+//        val timerFunction = {
+//            timerService.start(onTickEvent = {
+//                scrollScope.launch {
+//                    scrollState.animateScrollTo(
+//                        timerService.currentTimeInSeconds.value.toInt(DurationUnit.SECONDS),
+//                    )
+//                }
+//            }) {
+//                timerFinished = true
+//                timerService.end()
+//            }
+//        }
+//
+//        var initialized by remember { mutableStateOf(false) }
+//        if(!initialized) {
+//            timerService.setTime(1.minutes)
+//            initialized = true
+//        }
+//
+//        val hours by timerService.hours
+//        val minutes by timerService.minutes
+//        val seconds by timerService.seconds
+//
+//        val timerAdjustCoefficient = 0.01f
+//        var timerAdjustmentTick by remember { mutableFloatStateOf(0.0f) }
+//
+//        var momentum by remember { mutableFloatStateOf( 0.0f ) }
+//
+//        Column(
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//            Text(
+//                text = formatTime(hours, minutes, seconds),
+//                fontSize = 72.sp
+//            )
+//            Box(
+//            ){
+//                Row(
+//                    modifier = Modifier
+//                        .requiredWidth(300.dp)
+//                        .height(50.dp)
+//                        .horizontalScroll(scrollState),
+//                    verticalAlignment = Alignment.CenterVertically
+//                ) {
+//                    repeat(100) {
+//                        Text("  |  ", fontSize = 36.sp)
+//                        repeat(4) {
+//                            Text("  |  ", fontSize = 16.sp)
+//                        }
+//
+//                    }
+//                }
+//                Box(modifier = Modifier
+//                    .requiredWidth(300.dp)
+//                    .requiredHeight(50.dp)
+//                    .pointerInput(Unit) {
+//                        detectDragGestures (
+//                            onDragEnd = { timerAdjustmentTick = 0.0f }
+//                        ) { change, dragAmount ->
+//                            timerAdjustmentTick += dragAmount.x * timerAdjustCoefficient
+//                            if ( abs(timerAdjustmentTick) > 1) {
+//                                timerService.adjustTime(timerAdjustmentTick.toInt() * -60)
+//                                scrollScope.launch {
+//                                    scrollState.animateScrollTo(timerService.currentTimeInSeconds.value.toInt(
+//                                        DurationUnit.SECONDS))
+//                                }
+//                                timerAdjustmentTick = 0.0f
+//                            }
+//                        }
+//                    }
+//                )
+//            }
+//            Spacer(Modifier.height(20.dp))
+//            Column(
+//                horizontalAlignment = Alignment.CenterHorizontally
+//            )  {
+//                IconButton(
+//                    onClick = {
+//                        when (timerService.currentState.value) {
+//                            TimerService.State.Idle -> timerFunction()
+//                            TimerService.State.Running -> {
+//                                timerService.pause()
+//                            }
+//                            TimerService.State.Paused -> timerFunction()
+//                            TimerService.State.Reset -> timerFunction()
+//                        }
+//                    }
+//                ) {
+//                    val icon = when (timerService.currentState.value) {
+//                        TimerService.State.Idle -> painterResource(id = R.drawable.play_icon)
+//                        TimerService.State.Running -> painterResource(id = R.drawable.pause_icon)
+//                        TimerService.State.Paused ->  painterResource(id = R.drawable.play_icon)
+//                        TimerService.State.Reset ->  painterResource(id = R.drawable.play_icon)
+//                    }
+//                    Icon(
+//                        icon, "Play/Pause", Modifier.requiredSize(100.dp)
+//                    )
+//                }
+//
+//                IconButton(
+//                    enabled = timerService.currentState.value != TimerService.State.Idle &&
+//                            timerService.currentState.value != TimerService.State.Reset,
+//                    onClick = {
+//                        timerService.end()
+//                        initialized = false
+//                    }, modifier = Modifier.requiredSize(100.dp)
+//                ) {
+//                    Icon(
+//                        Icons.Filled.Refresh, "Play", Modifier.requiredSize(100.dp)
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}

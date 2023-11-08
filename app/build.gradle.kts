83234d0614f3748036411c49d8d7d9ae96d85b20
Paul Schiager <paulschi@stud.ntnu.no>
2023-11-08 14:38:25 +0100

    Added dependencies for ActivityRecognition API.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.3")
     implementation("androidx.navigation:navigation-compose:2.7.3")
     implementation("androidx.room:room-runtime:2.5.2")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     ksp("androidx.room:room-compiler:2.5.2")
     implementation("androidx.room:room-ktx:2.5.2")
     testImplementation("junit:junit:4.13.2")

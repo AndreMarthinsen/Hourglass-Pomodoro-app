81db94eb065683b0113e31af4f7132024239c4a3
Even Stetrud <evenstet@stud.ntnu.no>
2023-11-08 17:01:11 +0100

    Updated Edit-Screen with presetId-route argument, and ability to update existing presets

--- app/src/main/java/com/example/assignment1/ui/preset/PresetsViewModel.kt
+++ app/src/main/java/com/example/assignment1/ui/preset/PresetsViewModel.kt
@@ -1,15 +1,21 @@
 package com.example.assignment1.ui.preset
 
+import android.util.Log
+import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.assignment1.data.Preset
 import com.example.assignment1.data.PresetRepository
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
-class PresetsViewModel(presetRepository: PresetRepository) : ViewModel() {
+class PresetsViewModel(private val presetRepository: PresetRepository) : ViewModel() {
     val presetsUiState: StateFlow<PresetsUiState> =
         presetRepository.getAllPresetsStream().map { PresetsUiState(it) }
             .stateIn(
@@ -20,6 +26,16 @@ class PresetsViewModel(presetRepository: PresetRepository) : ViewModel() {
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    suspend fun deletePreset(presetId: Int) {
+        presetsUiState.value.presetList.find {preset->
+            preset.id == presetId
+        }?.let {
+            presetRepository.deletePreset(
+                it
+            )
+        }
     }
 }
 

e779f5147580f80054aba50a6d8ec10b0797ad81
Even Stetrud <evenstet@stud.ntnu.no>
2023-11-19 16:11:06 +0100

    Changed edit-screen styling

--- app/src/main/java/com/example/assignment1/ui/preset/PresetEditScreen.kt
+++ app/src/main/java/com/example/assignment1/ui/preset/PresetEditScreen.kt
@@ -1,8 +1,10 @@
 package com.example.assignment1.ui.preset
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,6 +13,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -19,6 +22,9 @@ import androidx.navigation.NavController
 import com.example.assignment1.PomodoroTopAppBar
 import com.example.assignment1.ui.AppViewModelProvider
 import com.example.assignment1.ui.navigation.NavigationDestination
+import com.example.assignment1.ui.visuals.MetallicContainer
+import com.example.assignment1.ui.visuals.RoundMetalButton
+import com.example.assignment1.ui.visuals.ShinyBlackContainer
 import kotlinx.coroutines.launch
 
 object PresetEditDestination : NavigationDestination {
@@ -63,17 +69,16 @@ fun PresetEditScreen(
        },
        modifier = modifier
     ) { innerPadding ->
-       PresetEntryBody(
-           presetUiState = viewModel.presetUiState,
-           onPresetValueChange = viewModel::updateUiState,
-           onSaveClick = {
-               onSaveClick()
-               navigateBack()
-           },
-           modifier = Modifier
-               .padding(innerPadding),
-           presetId = presetId
-       )
+        ShinyBlackContainer(modifier = Modifier.padding(innerPadding)) {
+            PresetEntryBody(
+                presetUiState = viewModel.presetUiState,
+                onPresetValueChange = viewModel::updateUiState,
+                onSaveClick = {
+                    onSaveClick()
+                    navigateBack()
+                }
+            )
+        }
     }
 }
 
@@ -82,24 +87,23 @@ fun PresetEntryBody(
     presetUiState: PresetUiState,
     onPresetValueChange: (PresetDetails) -> Unit,
     onSaveClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    presetId: Int
+    modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier.padding(top = 6.dp)
-    ) {
-        Text("PresetID: $presetId")
-        PresetEntryForm(
-            presetDetails = presetUiState.presetDetails,
-            onValueChange = onPresetValueChange,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Button(
-            onClick = onSaveClick,
-            enabled = presetUiState.isEntryValid,
-            modifier = Modifier.fillMaxWidth()
+    MetallicContainer(height = 600f, rounding = 6.dp) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Save preset")
+            Spacer(modifier = Modifier.size(20.dp))
+            PresetEntryForm(
+                presetDetails = presetUiState.presetDetails,
+                onValueChange = onPresetValueChange,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.size(20.dp))
+            RoundMetalButton(size = 120.dp, onClick = { onSaveClick() }) {
+                Text("Save preset")
+            }
+            Spacer(modifier = Modifier.size(20.dp))
         }
     }
 }
@@ -166,14 +170,5 @@ fun PresetEntryForm(
             enabled = enabled,
             singleLine = true
         )
-        /**
-         * TODO: decide if this hint is necessary, make it dynamically respond to missing fields
-         */
-        /*if (enabled) {
-            Text(
-                text = "Required fields"//stringResource(R.string.required_fields),
-                //modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
-            )
-        }*/
     }
 }
